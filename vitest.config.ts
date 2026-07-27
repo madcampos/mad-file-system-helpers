fb17ce8e5e3e899f13196f3a249de70d8a68ff2e
madcampos <609904+madcampos@users.noreply.github.com>
2026-07-27 17:18:40 -0400

    feat: update configuration

--- vitest.config.ts
+++ vitest.config.ts
@@ -15,12 +15,12 @@ export default defineConfig(() => ({
 		browser: {
 			enabled: true,
 			provider: playwright(),
-			// https://vitest.dev/config/browser/playwright
 			instances: [
 				{ browser: 'chromium' }
 			],
 			ui: false,
 			headless: true
-		}
+		},
+		open: false
 	}
 }));
